d7a3abab20e990b11d3000e4c57cf28dc649aba2
Mark Wicks <mawicks@gmail.com>
2013-03-13 01:12:53 -0400

    Simplify unit tests for character-set predicates.

--- pdf/characters_test.go
+++ pdf/characters_test.go
@@ -1,21 +1,38 @@
 package pdf
 
 import "testing"
+import "strings"
 
 // Unit tests follow
 
-func TestIsWhite (t *testing.T) {
-	for _,b := range []byte("\000\t\n\f\r ") {
-		if (!IsWhiteSpace (b)) {
-			t.Errorf ("IsWhiteSpace('\\%.3o') failed", b)
-		}
-	}
+func removeIf (f func (b byte) bool, s string) string {
+	return strings.Map (
+		func (r rune) rune {
+			if f(byte(r)) {
+				return r
+			}
+			return -1
+		},
+		s)
 }
 
-func TestIsDelimiter (t *testing.T) {
-	for _,b := range []byte("%()/<>[]{}") {
-		if (!IsDelimiter (b)) {
-			t.Errorf ("IsDelimiter('%c') failed", b)
-		}
+func TestAlt (t *testing.T) {
+	arrayOfAllLatin1Runes := make([]rune, 256, 256)
+
+	for i,_ := range arrayOfAllLatin1Runes {
+		arrayOfAllLatin1Runes[i] = rune(i)
+	}
+
+	stringOfAllLatin1Runes := string(arrayOfAllLatin1Runes)
+
+	justDelimiters := removeIf (IsDelimiter, stringOfAllLatin1Runes)
+	justWhite := removeIf (IsWhiteSpace, stringOfAllLatin1Runes)
+
+	if justDelimiters != "%()/<>[]{}" {
+		t.Errorf ("Incorrect delimiter character list: \"%s\"", justDelimiters)
+	}
+
+	if justWhite != "\000\t\n\f\r " {
+		t.Errorf ("Incorrect delimiter character list: \"%s\"", justWhite)
 	}
 }
