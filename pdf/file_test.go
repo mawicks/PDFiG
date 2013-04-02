a3b2d4ac6387d4aa149357f15ff5fc7eea79b6f0
Mark Wicks <mawicks@gmail.com>
2013-04-02 00:11:42 -0400

    Add File.AddObject() and File.DeleteObject() to unit test.

--- pdf/file_test.go
+++ pdf/file_test.go
@@ -4,6 +4,11 @@ import "testing"
 
 func TestTestFile (t *testing.T) {
 	f := NewFile("/tmp/foo.pdf")
+	obj1 := f.AddObject (NewNumeric(3.14))
+	obj2 := f.AddObject (NewNumeric(2.718))
+	f.DeleteObject (obj1)
+	f.AddObject (NewNumeric(3))
+	f.DeleteObject (obj2)
 	f.Close()
 }
 
