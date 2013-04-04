c121bf059153b3cec9704fecd9660ac250ee11b0
Mark Wicks <mawicks@gmail.com>
2013-04-04 00:04:44 -0400

    Add pdf.Rectangle class (specialization of pdf.Array).

---
+++ pdf/rectangle.go
@@ -0,0 +1,14 @@
+package pdf
+
+type Rectangle struct {
+	*Array
+}
+
+func NewRectangle(llx, lly, urx, ury float64) *Rectangle {
+	result := NewArray()
+	result.Add(NewNumeric(llx))
+	result.Add(NewNumeric(lly))
+	result.Add(NewNumeric(urx))
+	result.Add(NewNumeric(ury))
+	return &Rectangle{result}
+}
