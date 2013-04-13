31458623f84a224e2288b06c72a14f6d1d089ec0
Mark Wicks <mawicks@gmail.com>
2013-04-12 23:25:45 -0400

    Use mnemonics constants in os.File.Seek() calls.

--- readers/reverse_reader.go
+++ readers/reverse_reader.go
@@ -1,18 +1,19 @@
 package readers
 
 import "io"
+import "os"
 
 type ReverseReader struct {
 	readSeeker io.ReadSeeker
 }
 
 func NewReverseReader (readSeeker io.ReadSeeker) io.Reader {
-	readSeeker.Seek(0,2)
+	readSeeker.Seek(0,os.SEEK_END)
 	return &ReverseReader{readSeeker}
 }
 
 func (d *ReverseReader) Read (b []byte) (n int, err error) {
-	position,_ := d.readSeeker.Seek(0,1)
+	position,_ := d.readSeeker.Seek(0,os.SEEK_CUR)
 
 	if position == 0 {
 		return 0, io.EOF
@@ -25,14 +26,14 @@ func (d *ReverseReader) Read (b []byte) (n int, err error) {
 		min = len(b)
 	}
 
-	_,err = d.readSeeker.Seek(int64(-min),1)
+	_,err = d.readSeeker.Seek(int64(-min),os.SEEK_CUR)
 
 	if err == nil {
 		readBuffer := make([]byte, min, min)
 		n,err = d.readSeeker.Read (readBuffer)
 
 		if err == nil {
-			_,err = d.readSeeker.Seek(int64(-n), 1)
+			_,err = d.readSeeker.Seek(int64(-n), os.SEEK_CUR)
 			for i,v := range readBuffer {
 				b[min-i-1] = v
 			}
